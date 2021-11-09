ee97b3a897a900c1ecb6f4ea325edc5d117e5621
Ismael de Sousa <ismaeldesousa2014@outlook.com>
2021-11-09 20:03:06 -0300

    Deletado arquivo DOC

--- infoX/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"view","l":"Clientes"},{"p":"model","l":"DAO"},{"p":"view","l":"Login"},{"p":"view","l":"Principal"},{"p":"view","l":"Servico"},{"p":"view","l":"Sobre"},{"p":"view","l":"Usuarios"}]
